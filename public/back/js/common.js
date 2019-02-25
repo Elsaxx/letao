3b8cf954e99ebbc24d7288bc5743ec51d83cba01
Elsaxx <Elsaxx>
2019-02-25 10:31:21 +0800

    首页整体静态页面完成

--- public/back/js/common.js
+++ public/back/js/common.js
@@ -2,16 +2,16 @@
 //在第一个ajax发送出去时,开启进度条
 //在全部的ajax回来以后结束结束进度条
 
-$(document).ajaxStart(function(){
+$(document).ajaxStart(function () {
     //开启进度条
     NProgress.start();
 })
 
-$(document).ajaxStop(function(){
-   //结束进度条
-  // 模拟网络延迟,开启定时器
-   setTimeout( function(){
-    NPogress.done();
-   }, 500)
- 
+$(document).ajaxStop(function () {
+    //结束进度条
+    // 模拟网络延迟,开启定时器
+    setTimeout(function () {
+        NProgress.done();
+    }, 500)
+
 })
